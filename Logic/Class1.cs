6d2d747309fb951471be2d84a5a2651afb996ae3
Prerana Pawar <112629070+PreranaPawar28@users.noreply.github.com>
2025-07-20 23:33:05 +0530

    Add files via upload

---
+++ Logic/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using CRUD_Operation.Models;
+using System.Data;
+
+namespace CRUD_Operation.Logic
+{
+    public class Class1
+    {
+
+        public SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["PreranaPawar"].ConnectionString);
+
+        public List<EmpModel> GetAllData()
+        {
+            List<EmpModel> EmpList = new List<EmpModel>();
+            string qry = "select * from EmployeeData";
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            SqlDataAdapter SDA = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            conn.Open();
+            SDA.Fill(dt);
+            conn.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmpModel emp = new EmpModel()
+                {
+                    EmpId = Convert.ToInt32(dr["ID"]),
+                    EmpName = dr["EmpName"].ToString(),
+                    EmpSalary = dr["EmpSalary"].ToString(),
+                    EmpDesignation = dr["Designation"].ToString(),
+                    EmpCity = dr["City"].ToString()
+                };
+
+                EmpList.Add(emp);
+            }
+            return EmpList;
+        }
+
+        public string AddEmp(EmpModel emp)
+        {
+            string msg;
+            string qry = "insert into EmployeeData(EmpName,EmpSalary,Designation,City) values(@Name,@Salary,@EmpDesignation,@Empcity)";
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            cmd.Parameters.AddWithValue("@Name", emp.EmpName);
+            cmd.Parameters.AddWithValue("@Salary", emp.EmpSalary);
+            cmd.Parameters.AddWithValue("@EmpDesignation", emp.EmpDesignation);
+            cmd.Parameters.AddWithValue("@Empcity", emp.EmpCity);
+
+            conn.Open();
+            int i = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            if (i > 0)
+            {
+                msg = "Data Inserted Succesfully";
+            }
+            else
+            {
+                msg = "Insert fail";
+            }
+            return msg;
+        }
+    }
+}
---
+++ Models/EmpModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace CRUD_Operation.Models
+{
+    public class EmpModel
+    {
+        public int EmpId { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string EmpSalary { get; set; }
+
+        public string EmpDesignation { get; set; }
+
+        public string EmpCity { get; set; }
+    }
+}
